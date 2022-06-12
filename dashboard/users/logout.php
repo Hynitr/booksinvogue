1ddd40da615d9c3fff16278a6cbc34c5046adb26
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-06-12 02:39:54 +0100

    Create logout.php

---
+++ dashboard/users/logout.php
@@ -0,0 +1,17 @@
+<?php
+include('functions/init.php');
+
+$data = $_SESSION['login'];
+
+//get last seen
+$lastseen = date('Y-m-d h:i:sa');
+
+$sql = "UPDATE users SET `lastseen` = '$lastseen' WHERE `usname` = '$data'";
+$res = query($sql);
+
+//destroy session
+session_destroy();
+
+//redirect to login page
+redirect("./signin");
+?>
