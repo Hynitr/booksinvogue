0a8a148dc43d3cd270e43f14c514bc5e5953bcbb
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-06-13 17:16:24 +0100

    user dashboard

--- dashboard/users/components/navabr.php
+++ dashboard/users/components/navabr.php
@@ -11,8 +11,8 @@
         <div class="navbar-nav align-items-center">
             <div class="nav-item d-flex align-items-center">
                 <i class="bx bx-search fs-4 lh-0"></i>
-                <input type="text" class="form-control border-0 shadow-none" placeholder="Search..."
-                    aria-label="Search..." />
+                <input type="text" class="form-control border-0 shadow-none" placeholder="Search for a book..."
+                    aria-label="Search for a book.." />
             </div>
         </div>
         <!-- /Search -->
