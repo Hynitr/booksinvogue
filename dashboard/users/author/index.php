9b9f97db75d219e68cea2403a1be1c1a91cd39db
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-06-21 04:06:41 +0100

    banks

--- dashboard/users/author/index.php
+++ dashboard/users/author/index.php
@@ -32,6 +32,304 @@ user_details();
                     <div class="container-xxl flex-grow-1 container-p-y">
                         <div class="row">
 
+                            <?php
+                            if($t_users['act no'] == null) {
+
+                                ?>
+
+                            <div id="signup">
+                                <form id="formAuthentication" class="mb-3" method="POST" autocomplete="off">
+                                    <h4 class="mb-4">How should we credit you? 🚀</h4>
+                                    <div class="mb-3">
+                                        <label for="fullname" class="form-label">Account Number</label>
+                                        <input type="text" class="form-control" id="fname" name="fname"
+                                            placeholder="Firstname Lastname" autofocus />
+                                        <h6 style="font-size: 12px" class="text-danger mt-1" id="fmsg"></h6>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="fullname" class="form-label">Select your bank name</label>
+
+                                        <select id="bank" class="form-control">
+                                            <?php
+
+$banks = array(
+array('id' => '1','name' => 'Access Bank','code'=>'044'),
+array('id' => '2','name' => 'Citibank','code'=>'023'),
+array('id' => '3','name' => 'Diamond Bank','code'=>'063'),
+array('id' => '4','name' => 'Dynamic Standard Bank','code'=>''),
+array('id' => '5','name' => 'Ecobank Nigeria','code'=>'050'),
+array('id' => '6','name' => 'Fidelity Bank Nigeria','code'=>'070'),
+array('id' => '7','name' => 'First Bank of Nigeria','code'=>'011'),
+array('id' => '8','name' => 'First City Monument Bank','code'=>'214'),
+array('id' => '9','name' => 'Guaranty Trust Bank','code'=>'058'),
+array('id' => '10','name' => 'Heritage Bank Plc','code'=>'030'),
+array('id' => '11','name' => 'Jaiz Bank','code'=>'301'),
+array('id' => '12','name' => 'Keystone Bank Limited','code'=>'082'),
+array('id' => '13','name' => 'Providus Bank Plc','code'=>'101'),
+array('id' => '14','name' => 'Polaris Bank','code'=>'076'),
+array('id' => '15','name' => 'Stanbic IBTC Bank Nigeria Limited','code'=>'221'),
+array('id' => '16','name' => 'Standard Chartered Bank','code'=>'068'),
+array('id' => '17','name' => 'Sterling Bank','code'=>'232'),
+array('id' => '18','name' => 'Suntrust Bank Nigeria Limited','code'=>'100'),
+array('id' => '19','name' => 'Union Bank of Nigeria','code'=>'032'),
+array('id' => '20','name' => 'United Bank for Africa','code'=>'033'),
+array('id' => '21','name' => 'Unity Bank Plc','code'=>'215'),
+array('id' => '22','name' => 'Wema Bank','code'=>'035'),
+array('id' => '23','name' => 'Zenith Bank','code'=>'057'),
+array('id' => '24','name' => 'HighStreet MFB bank','code'=>'090175'),
+array('id' => '25','name' => 'TCF MFB','code' => '90115'),
+array(
+'id' => 132,
+'code' => '560',
+'name' => 'Page MFBank'
+),
+array(
+'id' => 133,
+'code' => '304',
+'name' => 'Stanbic Mobile Money'
+),
+array(
+'id' => 134,
+'code' => '308',
+'name' => 'FortisMobile'
+),
+array(
+'id' => 135,
+'code' => '328',
+'name' => 'TagPay'
+),
+array(
+'id' => 136,
+'code' => '309',
+'name' => 'FBNMobile'
+),
+array(
+'id' => 137,
+'code' => '011',
+'name' => 'First Bank of Nigeria'
+),
+array(
+'id' => 138,
+'code' => '326',
+'name' => 'Sterling Mobile'
+),
+array(
+'id' => 139,
+'code' => '990',
+'name' => 'Omoluabi Mortgage Bank'
+),
+array(
+'id' => 140,
+'code' => '311',
+'name' => 'ReadyCash (Parkway)'
+),
+array(
+'id' => 143,
+'code' => '306',
+'name' => 'eTranzact'
+),
+array(
+'id' => 145,
+'code' => '023',
+'name' => 'CitiBank'
+),
+array(
+'id' => 147,
+'code' => '323',
+'name' => 'Access Money'
+),
+array(
+'id' => 148,
+'code' => '302',
+'name' => 'Eartholeum'
+),
+array(
+'id' => 149,
+'code' => '324',
+'name' => 'Hedonmark'
+),
+array(
+'id' => 150,
+'code' => '325',
+'name' => 'MoneyBox'
+),
+array(
+'id' => 151,
+'code' => '301',
+'name' => 'JAIZ Bank'
+),
+array(
+'id' => 153,
+'code' => '307',
+'name' => 'EcoMobile'
+),
+array(
+'id' => 154,
+'code' => '318',
+'name' => 'Fidelity Mobile'
+),
+array(
+'id' => 155,
+'code' => '319',
+'name' => 'TeasyMobile'
+),
+array(
+'id' => 156,
+'code' => '999',
+'name' => 'NIP Virtual Bank'
+),
+array(
+'id' => 157,
+'code' => '320',
+'name' => 'VTNetworks'
+),
+array(
+'id' => 159,
+'code' => '501',
+'name' => 'Fortis Microfinance Bank'
+),
+array(
+'id' => 160,
+'code' => '329',
+'name' => 'PayAttitude Online'
+),
+array(
+'id' => 161,
+'code' => '322',
+'name' => 'ZenithMobile'
+),
+array(
+'id' => 162,
+'code' => '303',
+'name' => 'ChamsMobile'
+),
+array(
+'id' => 163,
+'code' => '403',
+'name' => 'SafeTrust Mortgage Bank'
+),
+array(
+'id' => 164,
+'code' => '551',
+'name' => 'Covenant Microfinance Bank'
+),
+array(
+'id' => 165,
+'code' => '415',
+'name' => 'Imperial Homes Mortgage Bank'
+),
+array(
+'id' => 166,
+'code' => '552',
+'name' => 'NPF MicroFinance Bank'
+),
+array(
+'id' => 167,
+'code' => '526',
+'name' => 'Parralex'
+),
+array(
+'id' => 169,
+'code' => '084',
+'name' => 'Enterprise Bank'
+),
+array(
+'id' => 187,
+'code' => '314',
+'name' => 'FET'
+),
+array(
+'id' => 188,
+'code' => '523',
+'name' => 'Trustbond'
+),
+array(
+'id' => 189,
+'code' => '315',
+'name' => 'GTMobile'
+),
+array(
+'id' => 182,
+'code' => '327',
+'name' => 'Pagatech'
+),
+array(
+'id' => 183,
+'code' => '559',
+'name' => 'Coronation Merchant Bank'
+),
+array(
+'id' => 184,
+'code' => '601',
+'name' => 'FSDH'
+),
+array(
+'id' => 185,
+'code' => '313',
+'name' => 'Mkudi'
+),
+array(
+'id' => 171,
+'code' => '305',
+'name' => 'Paycom'
+),
+array(
+'id' => 172,
+'code' => '100',
+'name' => 'SunTrust Bank'
+),
+array(
+'id' => 173,
+'code' => '317',
+'name' => 'Cellulant'
+),
+array(
+'id' => 174,
+'code' => '401',
+'name' => 'ASO Savings and & Loans'
+),
+array(
+'id' => 176,
+'code' => '402',
+'name' => 'Jubilee Life Mortgage Bank'
+),
+);
+
+                                            $row = 0; 
+
+                                            while($row < 68) {
+
+                                            echo '<option id="bank">'.$banks[$row]['name'].'</option>';
+
+                                            $row++;
+                                            } 
+                                            ?>
+                                        </select>
+                                        <h6 style="font-size: 12px" class="text-danger mt-1" id="fmsg"></h6>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Account Name</label>
+                                        <input type="email" class="form-control" id="email" name="email"
+                                            placeholder="Enter your email" disabled />
+                                        <h6 style="font-size: 12px" class="text-danger mt-1" id="emmsg"></h6>
+                                    </div>
+
+                                    <h6 style="font-size: 13px" class="text-danger  text-center mt-1" id="msg"></h6>
+                                    <button type="button" id="sub" class="mb-3 btn btn-primary d-grid w-100"
+                                        disabled>Submit</button>
+
+                                </form>
+                            </div>
+                            <?php
+
+
+                    } else {
+
+                    ?>
+
+
+
                             <div class="col-lg-12 col-md-12 col-sm-12 order-0">
                                 <div class="row">
 
@@ -46,7 +344,7 @@ user_details();
                                                     </div>
 
                                                 </div>
-                                                <span class="fw-semibold d-block mb-1">Total Books Sold</span>
+                                                <span class="fw-semibold d-block mb-1">Total Book(s) Sold</span>
                                                 <h3 class="card-title mb-3">
                                                     <?php echo number_format($t_users['wallet']) ?></h3>
                                                 <a href="javascript:;" class="btn btn-sm btn-outline-primary">View
@@ -97,6 +395,7 @@ user_details();
 
                                 </div>
                             </div>
+
                         </div>
 
 
@@ -115,7 +414,8 @@ user_details();
                                                         <h5 class="text-nowrap mb-5">Total Books Bought</h5>
                                                         <h2 class="mb-4">8,258</h2>
                                                         <a href="javascript:;"
-                                                            class="btn btn-sm btn-outline-primary">Buy New Book</a>
+                                                            class="btn btn-sm btn-outline-primary">Buy New
+                                                            Book</a>
                                                     </div>
 
                                                 </div>
@@ -158,8 +458,12 @@ user_details();
                         </div>
                         <!--/ Total Revenue -->
 
+                        <?php
+                            }
+                            ?>
 
                     </div>
+
 
                 </div>
                 <!-- / Content -->
