b87dbb03baaaee573ada04180792ea21c7ee95de
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-07-18 14:24:18 +0100

    final

--- dashboard/users/final.php
+++ dashboard/users/final.php
@@ -27,12 +27,12 @@
     <body style="background-color: #eaebed;  font-family: sans-serif;  font-size: 14px; line-height: 1.4; margin-bottom: 2rem !important; padding: 0;">
 
     
-    <div style="text-align: center !important; justify-content: center !important; margin-top: 24px !important;">
-    <img style="max-width: 100%; height: auto; vertical-align: middle; box-sizing: border-box; width: 120px;" src="https://dashboard.booksinvogue.com.ng/assets/img/logo.png">
+    <div style="text-align: center !important; justify-content: center !important;">
+    <img style="max-width: 100%; height: auto; vertical-align: middle; box-sizing: border-box; width: 120px; margin-top: 24px !important;" src="https://dashboard.booksinvogue.com.ng/assets/img/logo.png">
     </div>
 
     <div style="margin-right: 5%; margin-left: 5%;">
-        <tr><p></p></tr>
+        
         <div style="padding-right: 1.105rem; padding-left: 1.105rem; margin-top: 24px !important; background-color: #fff; position: relative; display: flex; flex-direction: column; height: auto; word-wrap: break-word; background-clip: border-box; border: 0 solid #d9dee3; border-radius: 8px;">
        
         <tr>
